f2642b502813a384d026b849612bb1c25d675613
delba <delba@users.noreply.github.com>
2015-03-02 17:40:24 -0500

    Create bins

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
+	"strings"
 	"text/template"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 
 	"github.com/delba/requestbin/model"
@@ -18,40 +21,6 @@ var db gorm.DB
 func handle(err error) {
 	if err != nil {
 		panic(err)
-	}
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	var requests []model.Request
-	db.Find(&requests)
-
-	layout := path.Join("templates", "layouts", "application.html")
-	index := path.Join("templates", "bins", "index.html")
-	t, err := template.ParseFiles(layout, index)
-
-	handle(err)
-
-	err = t.Execute(w, requests)
-	handle(err)
-}
-
-func create(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	handle(err)
-	defer r.Body.Close()
-
-	request := model.Request{Body: body}
-	db.Create(&request)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		index(w, r)
-	case "POST":
-		create(w, r)
-	default:
-		index(w, r)
 	}
 }
 
@@ -67,7 +36,12 @@ func init() {
 		return db
 	}()
 
+	// db.DropTable(&model.Bin{})
+	// db.DropTable(&model.Request{})
+	db.CreateTable(&model.Bin{})
 	db.CreateTable(&model.Request{})
+
+	db.LogMode(true)
 }
 
 func main() {
@@ -76,6 +50,101 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":"+port, nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", BinsIndex).
+		Methods("GET")
+
+	r.HandleFunc("/bins", BinsCreate).
+		Methods("POST")
+
+	r.HandleFunc("/{token}", BinsShow).
+		Methods("GET")
+
+	r.HandleFunc("/{token}", RequestsCreate).
+		Methods("POST")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
+
+func BinsIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("bins#index")
+
+	cookie, _ := r.Cookie("tokens")
+	if cookie == nil {
+		cookie = &http.Cookie{Name: "tokens"}
+	}
+
+	layout := path.Join("templates", "layouts", "application.html")
+	index := path.Join("templates", "bins", "index.html")
+	t, err := template.ParseFiles(layout, index)
+	handle(err)
+
+	err = t.Execute(w, nil)
+	handle(err)
+}
+
+func BinsCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("bins#create")
+
+	var bin model.Bin
+	db.Create(&bin)
+
+	cookie, _ := r.Cookie("tokens")
+	if cookie == nil {
+		cookie = &http.Cookie{Name: "tokens"}
+	}
+
+	fmt.Println(cookie.Value)
+	cookie.Value = strings.Join([]string{cookie.Value, bin.Token}, ", ")
+	fmt.Println(cookie.Value)
+
+	http.SetCookie(w, cookie)
+
+	http.Redirect(w, r, "/"+bin.Token, 302)
+}
+
+func BinsShow(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("bins#show")
+
+	bin := findBin(r)
+	fmt.Println(bin)
+
+	var requests []model.Request
+	db.Model(&bin).Related(&requests)
+
+	layout := path.Join("templates", "layouts", "application.html")
+	show := path.Join("templates", "bins", "show.html")
+	t, err := template.ParseFiles(layout, show)
+	handle(err)
+
+	err = t.Execute(w, requests)
+	handle(err)
+}
+
+func RequestsCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("requests#create")
+
+	bin := findBin(r)
+	fmt.Println(bin)
+
+	body, err := ioutil.ReadAll(r.Body)
+	handle(err)
+	defer r.Body.Close()
+
+	fmt.Println("Bin id is", bin.ID)
+	request := model.Request{Body: body, BinID: bin.ID}
+	db.Create(&request)
+
+	http.Redirect(w, r, "/"+bin.Token, 302)
+}
+
+func findBin(r *http.Request) model.Bin {
+	token := mux.Vars(r)["token"]
+
+	var bin model.Bin
+	db.Where(&model.Bin{Token: token}).First(&bin)
+
+	return bin
 }
--- model/model.go
+++ model/model.go
@@ -2,13 +2,47 @@ package model
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 )
 
+type Bin struct {
+	ID       int
+	Token    string `sql:"unique"`
+	Requests []Request
+}
+
+func (b *Bin) BeforeCreate() (err error) {
+	// TODO loop to make sure that the token is unique
+
+	buf := make([]byte, 4)
+
+	if _, err = io.ReadFull(rand.Reader, buf); err != nil {
+		err = err
+	}
+
+	b.Token = hex.EncodeToString(buf)
+
+	return err
+}
+
+func (b *Bin) GenerateToken() string {
+	buf := make([]byte, 4)
+
+	if _, err := io.ReadFull(rand.Reader, buf); err != nil {
+		fmt.Println(err)
+	}
+
+	return hex.EncodeToString(buf)
+}
+
 type Request struct {
-	ID   int
-	Body []byte
+	ID    int
+	Body  []byte
+	BinID int
 }
 
 func (r *Request) FormattedBody() string {
